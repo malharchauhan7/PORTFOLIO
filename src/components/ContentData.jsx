993820a702be1f8e660fdd6e27391c80e8636d8c
malharchauhan7 <malharchauhan02@gmail.com>
2024-08-17 00:58:37 +0530

    First Article Added

--- src/components/ContentData.jsx
+++ src/components/ContentData.jsx
@@ -2,51 +2,109 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { motion } from "framer-motion";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const ContentData = ({ data }) => {
   return (
-    <motion.div className="flex flex-col gap-4 md:gap-6 p-4 md:p-6">
+    <motion.div className="flex flex-col gap-4 md:gap-5 p-4 md:p-6">
       {/* Date */}
       <div className="text-base md:text-lg text-gray-600">{data.date}</div>
       {/* Title */}
-      <div className="text-2xl md:text-4xl font-bold">{data.title}</div>
+
+      <div className="flex gap-2">
+        <div className="text-2xl md:text-4xl font-bold">{data.title}</div>
+        {/* {link} */}
+        <a
+          href={data.link}
+          target="_blank"
+          className="font-semibold text-blue-500 hover:text-blue-800 transition-colors mt-2"
+        >
+          <FaExternalLinkAlt size={15} />
+        </a>
+      </div>
       {/* Subtitle */}
       <div className="text-lg md:text-xl font-medium text-gray-800">
         {data.subtitle}
       </div>
+
       {/* Image */}
       <div className="rounded-md overflow-hidden my-4">
         <img
           src={data.image}
           alt={data.title}
-          className="w-2/3 h-auto object-cover"
+          className="w-auto h-auto object-cover"
         />
       </div>
+
       {/* Content */}
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section1}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section2}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section3}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section3}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section4}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section5}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section6}
-      </div>
-      <div className="text-base md:text-lg text-gray-700 my-2">
-        {data.content.section7}
-      </div>
+      {data && (
+        <>
+          <div className="text-base md:text-xl text-gray-700 my-2">
+            {data.content.section1}
+          </div>
+          <div className="text-base md:text-xl text-gray-700  font-semibold">
+            {data.content.subtitle2}
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-1"> {data.content.section2.h1}</div>
+            <div>{data.content.section2.paragraph1}</div>
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-1"> {data.content.section4.h1}</div>
+            <div>{data.content.section4.paragraph1}</div>
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-1"> {data.content.section5.h1}</div>
+            <div>{data.content.section5.paragraph1}</div>
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-1"> {data.content.section6.h1}</div>
+            <div>{data.content.section6.paragraph1}</div>
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-3 font-semibold">{data.content.section7.h1}</div>
+            <div>{data.content.section7.paragraph1}</div>
+          </div>
+          <div className="text-base md:text-xl text-gray-700 my-1 ">
+            <div className="my-3 font-semibold">{data.content.section8.h1}</div>
+            <div>
+              <div className="my-3">{data.content.section8.points.h2}</div>
+              <div className="my-2">
+                {data.content.section8.points.paragraph1}
+              </div>
+              <div className="my-2">
+                {data.content.section8.points.paragraph2}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-xl font-semibold my-3">
+              {data.content.conclusion.h1}
+            </div>
+            <div className="text-xl my-2">
+              {data.content.conclusion.paragraph1}
+            </div>
+            <div className="text-xl my-1">
+              {data.content.conclusion.paragraph2}
+            </div>
+          </div>
+          <div>
+            <h2 className="">Thank you for Reading</h2>
+          </div>
+          <div>
+            <a
+              href={data.link}
+              target="_blank"
+              className="flex gap-2 font-semibold text-blue-500 hover:text-blue-800 transition-colors"
+            >
+              Link
+              <div className="mt-2">
+                <FaExternalLinkAlt size={12} />
+              </div>
+            </a>
+          </div>
+        </>
+      )}
     </motion.div>
   );
 };
--- src/newsletter.js
+++ src/newsletter.js
@@ -1,21 +1,65 @@
 const newsdata = [
-  // {
-  //   id: 1,
-  //   date: "10.08.24",
-  //   title: "First Article",
-  //   subtitle: "subtitle1",
-  //   image: "src/images/logo.png",
-  //   content: {
-  //     section1: "section1",
-  //     section2: "section2",
-  //     section3: "section3",
-  //     section4: "section4",
-  //     section5: "section5",
-  //     section6: "section6",
-  //     section7: "section7",
-  //   },
-  //   link: "link1",
-  // },
+  {
+    id: 1,
+    date: "16.08.24",
+    title: "Neumorphism in Google Drive UI Design",
+    subtitle: "Exploring the Modern Fusion of Flat Design and Skeuomorphism.",
+    image: "src/images/newsletter/driveredesign.png",
+    link:
+      "https://www.figma.com/community/file/1406357600679855634/google-drive-neumorphism",
+    content: {
+      section1:
+        "Neumorphism is a modern UI design trend that blends elements of flat design with skeuomorphism. It uses soft shadows, gradients, and rounded corners to give digital elements a tactile, three-dimensional feel while maintaining a minimalist aesthetic.",
+      subtitle2: "Key Features of the Neumorphic Google Drive UI",
+      section2: {
+        h1: "1. Soft Shadows and Highlights:",
+        paragraph1:
+          "Dual shadows create depth, making icons, file cards, and buttons appear slightly raised from the background, enhancing their tactile feel.",
+      },
+      section3: {
+        h1: "2. Minimalistic Color Palette:",
+        paragraph1:
+          "A clean, neutral color scheme with subtle color hints keeps the interface modern and visually calm, focusing on usability without overwhelming the user.",
+      },
+      section4: {
+        h1: "3. Rounded Corners:",
+        paragraph1:
+          "The design uses rounded corners to enhance the soft, approachable feel, aligning with Neumorphism’s goal of creating a user-friendly experience",
+      },
+      section5: {
+        h1: "4. Tactile Interaction:",
+        paragraph1:
+          "Elements like buttons and icons have a raised appearance, making them intuitive to interact with, almost as if they can be physically pressed.",
+      },
+      section6: {
+        h1: "5. Consistent Design:",
+        paragraph1:
+          "The entire interface follows a cohesive Neumorphic style, making the user experience seamless and visually connected.",
+      },
+      section7: {
+        h1: "User Experience Impact",
+        paragraph1:
+          "Neumorphism enhances the user experience by making the interface feel intuitive and engaging. The subtle shadows guide the user's attention, highlighting key features without being distracting.",
+      },
+      section8: {
+        h1: "Challenges",
+        points: {
+          h2: "While visually appealing, Neumorphism can present challenges:",
+          paragraph1:
+            "Accessibility: Subtle shadows and gradients may be difficult for users with visual impairments. Ensuring contrast and alternative interaction cues is essential.",
+          paragraph2:
+            "Scalability: Maintaining design integrity across different devices can be challenging due to the detailed shadowing and gradients.",
+        },
+      },
+      conclusion: {
+        h1: "Conclusion",
+        paragraph1:
+          "The Neumorphic design of Google Drive is a fresh take on UI aesthetics, making interfaces feel both modern and tangible. While there are challenges, particularly with accessibility, Neumorphism has the potential to enhance user interaction by making digital elements more engaging and intuitive.",
+        paragraph2:
+          "This design approach shows that visual appeal and functionality can go hand in hand, creating tools that are not just practical but also enjoyable to use.",
+      },
+    },
+  },
 ];
 
 export default newsdata;
