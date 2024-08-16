c8be856fb3647e63fb25c5e6cc58b0b4c27f7b7c
malharchauhan7 <malharchauhan02@gmail.com>
2024-08-16 16:56:03 +0530

    Updateed

--- src/data.js
+++ src/data.js
@@ -1,5 +1,13 @@
 const data = [
   {
+    title: "File Converter",
+    year: "2024",
+    Tech: "React TailwinCSS DaisyUI",
+    link: "https://file-converter-xi.vercel.app/",
+    content:
+      "Easily convert PNG images to JPG, JPEG, WEBP, PDF, GIF or AVIF formats and JPG/JPEG images to PNG.",
+  },
+  {
     title: "TatvaSoft-Internship",
     year: "2024",
     Tech: "Virtual Community Support ",
@@ -13,14 +21,6 @@ const data = [
     Tech: "Product & Graphics Design",
     link: "https://dribbins.etsy.com/",
     content: "Creating Products and Graphics for DRIBBINS",
-  },
-  {
-    title: "File Converter",
-    year: "2024",
-    Tech: "React TailwinCSS DaisyUI",
-    link: "https://file-converter-xi.vercel.app/",
-    content:
-      "Easily convert PNG images to JPG, JPEG, WEBP, PDF, GIF or AVIF formats and JPG/JPEG images to PNG.",
   },
   {
     title: "Job-Nodes",
