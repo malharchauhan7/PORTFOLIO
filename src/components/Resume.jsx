f816251f569c9d6a2954d57442c53637aaf008fe
malharchauhan7 <malharchauhan02@gmail.com>
2025-03-31 21:24:26 +0530

    Resume changes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+//https://drive.google.com/file/d/1jiKgzeiR-86gLXfDf5Eyc7X3dOIs9V3O/view?usp=sharing
 const Resume = () => {
   return (
     <div>
-      <a href="https://drive.google.com/file/d/1jiKgzeiR-86gLXfDf5Eyc7X3dOIs9V3O/view?usp=sharing">
-        <motion.button
-          className="btn btn-xs btn-neutral"
-          whileHover={{ scale: 1.03 }}
-        >
+      <a href="https://malharchauhan7.github.io/resume.cv/">
+        <motion.button className="btn btn-xs " whileHover={{ scale: 1.05 }}>
           Resume
         </motion.button>
       </a>
